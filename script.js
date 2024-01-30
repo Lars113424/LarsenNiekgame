7f28c01670df0ed655e867a6fa8f7056e9dc0884
Lars113424 Lars <113424@emmauscollege.nl>
2024-01-30 11:22:45 +0000

    30 jan

--- script.js
+++ script.js
@@ -18,9 +18,12 @@
 const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
+var bullet;
+var tank;
 
 var spelerX = 600; // x-positie van speler
-var spelerY = 600; // y-positie van speler
+var spelerY = 600;
+ // y-positie van speler
 
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
@@ -60,12 +63,10 @@ var tekenAlles = function() {
   // vijand
 
   // kogel
+  image(bullet, 100, 100, 80, 80);
 
   // speler
-  fill("white");
-  rect(spelerX - 25, spelerY - 25, 50, 50);
-  fill("black");
-  ellipse(spelerX, spelerY, 10, 10);
+  image(tank, 200, 200);
 
   // punten en health
 
@@ -89,6 +90,11 @@ var checkGameOver = function() {
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
+function preload() {
+  bullet = loadImage('bullet.png');
+  tank = loadImage('tank.png')
+}
+
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
