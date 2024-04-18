252b9665a0a9b1127f2a474ab6080a944403b2c0
Lars113424 Lars <113424@emmauscollege.nl>
2024-04-18 13:30:39 +0000

    do 18 apr

--- script.js
+++ script.js
@@ -18,14 +18,30 @@
 const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
+
+//afbeeldingen
 var backimage;
 var cookie;
+var BuildingButton;
+var muis;
+
+//cookie variabelen 
 let AmountCookies = 0;
+let CPS = 0;
 
-//var achtergrond;
 
-var spelerX = 0; // x-positie van speler
-var spelerY = 100;
+
+//cookies per seconde
+function addCookiesPerSecond() {
+  AmountCookies = AmountCookies + CPS; 
+  console.log("Aantal cookies: " + AmountCookies); // Uitvoer naar de console, je kunt dit naar wens aanpassen
+}
+//speelt functie elke seconde af
+ setInterval(addCookiesPerSecond, 1000);
+
+//cookie click functie
+
+
 
 
 var mouseClicked = function() {
@@ -33,36 +49,21 @@ var mouseClicked = function() {
       AmountCookies = AmountCookies + 1;
       //geeft een cookie per click
   }
+
+  if (mouseX > 410 && mouseX < 925 && mouseY > 145 && mouseY < 280) { 
+    if (AmountCookies - 10 >= 0)  {
+     CPS++;
+      AmountCookies = AmountCookies - 10;
+    }
+  }
 };
-/**
- * Checkt botsingen
- * Verwijdert neergeschoten dingen
- * Updatet globale variabelen punten en health
- */
-
-
-/**
- * Tekent spelscherm
- */
-
-
 
 var tekenAlles = function() {
   // achtergrond
   image(backimage, 0, 0, 1280, 720);
 
 
-  // vijand
-
-  // kogel
-
-  // speler
  
- 
-
-
-
-  // punten en health
  
 };
 
@@ -70,22 +71,38 @@ var design = function() {
   //cookie
   image(cookie, 40, 100, 300, 300);
   //lines
-  line(405, 0, 405, 720,);
-  line(1108, 0, 1108, 720,);
   strokeWeight(10);
+  line(405, 0, 405, 720,);
+  line(930, 0, 930, 720,);
+  
+  strokeWeight(5);
+  line(1280, 140, 0, 140);
+  
+  //locatie test (later weghalen)
+  textSize(50);
+  text(mouseX, 1000, 80);
+  text(mouseY, 1000, 120);
+ 
+
   //cookie text
-  text("Cookie's Eaten;", 40, 60)
-  text(AmountCookies, 190, 100)
   textSize(46);
+  text("Cookie's Eaten;", 40, 50);
+  text(AmountCookies, 190, 85);
+  
  //upgrades
+  image(BuildingButton, 410, 50, 514, 350);
+  image(muis, 420, 120, 200, 200);
+
+ //upgrade text
+  textSize(30);
+  text("Mouse; 10 cookies", 560, 210);
+  textSize(20);
+  text("cookie's per second; 0.5", 561, 240)
 
  //gebouwen
 
 };
-/**
- * return true als het gameover is
- * anders return false
- */
+
 var checkGameOver = function() {
   // check of HP 0 is , of tijd op is, of ...
   return false;
@@ -103,6 +120,8 @@ var checkGameOver = function() {
 function preload() {
   backimage = loadImage('afbeeldingen/background.png');
   cookie = loadImage('afbeeldingen/cookie.png');
+  BuildingButton = loadImage('afbeeldingen/button.png');
+  muis = loadImage('afbeeldingen/Muis.png');
   
   //achtergrond = loadImage ('achtergrond.png');
 
